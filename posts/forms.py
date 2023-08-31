c331ae21d1f572082602db31385cb342df8b5490
yejuPark <pyj8977@daum.net>
2023-08-31 15:13:34 +0900

    fin

--- posts/forms.py
+++ posts/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
   class Meta:
     model = Post
-    exclude = ('user', )
+    exclude = ('user', 'like_users', )
 
 
 class CommentForm(forms.ModelForm):
