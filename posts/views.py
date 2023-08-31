95ea03326ef461e214e91bcf722ee04e8b272956
yejuPark <pyj8977@daum.net>
2023-08-31 10:17:00 +0900

    login_required

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,6 +16,7 @@ def index(request):
   return render(request, 'index.html', context)
 
 
+@login_required
 def create(request):
   if request.method == 'POST':
     form = PostForm(request.POST, request.FILES)
@@ -33,6 +35,7 @@ def create(request):
   return render(request, 'form.html', context)
 
 
+@login_required
 def comment_create(request, post_id):
   comment_form = CommentForm(request.POST)
 
@@ -47,4 +50,3 @@ def comment_create(request, post_id):
 
     comment.save()
     return redirect('posts:index')
-
