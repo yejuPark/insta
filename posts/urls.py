7b8a5f85f2ddf11f1b9873dc43f1a75177a5ff34
yejuPark <pyj8977@daum.net>
2023-09-05 16:02:38 +0900

    like-js

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('create/', views.create, name='create'),
   path('<int:post_id>/comments/create/', views.comment_create, name='comment_create'),
   path('<int:post_id>/like/', views.like, name='like'),
+  path('<int:post_id>/like-async/', views.like_async, name='like_async'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -67,3 +68,27 @@ def like(request, post_id):
   # user.like_posts.add(post)
 
   return redirect('posts:index')
+
+
+
+
+def like_async(request, post_id):
+  # context = {
+  #   'message' : post_id,
+  # }
+  user = request.user
+  post = Post.objects.get(id=post_id)
+
+  if user in post.like_users.all():
+    post.like_users.remove(user)
+    status = False
+  else:
+    post.like_users.add(user)
+    status = True
+  
+  context = {
+    'status': status,
+    'count': len(post.like_users.all())
+  }
+
+  return JsonResponse(context)
